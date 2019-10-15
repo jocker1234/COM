7bf184ea87c68bc706c6c4c5b449a23ccde40311
pgogol26@gmail.com <Qzwsdcrf12>
2019-10-15 22:14:42 +0200

    export preparation

---
+++ backend/src/main/java/com/com/backend/batch/writer/ResearchAbstractsWriter.java
@@ -0,0 +1,15 @@
+package com.com.backend.batch.writer;
+
+import com.com.backend.model.ResearchAbstracts;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class ResearchAbstractsWriter implements ItemWriter<ResearchAbstracts> {
+
+    @Override
+    public void write(List<? extends ResearchAbstracts> list) throws Exception {
+
+    }
+
+}
