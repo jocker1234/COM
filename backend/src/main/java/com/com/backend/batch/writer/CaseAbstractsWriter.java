82c87aac9bbb1fa9e706ace78aa5a13bb030e3dd
pgogol26@gmail.com <Qzwsdcrf12>
2019-10-15 22:14:42 +0200

    export preparation

---
+++ backend/src/main/java/com/com/backend/batch/writer/CaseAbstractsWriter.java
@@ -0,0 +1,15 @@
+package com.com.backend.batch.writer;
+
+import com.com.backend.model.CaseAbstracts;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class CaseAbstractsWriter implements ItemWriter<CaseAbstracts> {
+
+    @Override
+    public void write(List<? extends CaseAbstracts> list) throws Exception {
+
+    }
+
+}
