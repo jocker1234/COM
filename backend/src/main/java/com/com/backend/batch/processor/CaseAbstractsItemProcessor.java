a26e126ebce5250cf909de50504d0c6a4d29b1bd
pgogol26@gmail.com <Qzwsdcrf12>
2019-10-16 23:21:33 +0200

    prepare method in reader, process, write and etc in batch

--- backend/src/main/java/com/com/backend/batch/processor/CaseAbstractsItemProcessor.java
+++ backend/src/main/java/com/com/backend/batch/processor/CaseAbstractsItemProcessor.java
@@ -1,17 +1,28 @@
 package com.com.backend.batch.processor;
 
+import com.com.backend.dao.CaseAbstractsDao;
 import com.com.backend.model.CaseAbstracts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
-public class CaseAbstractsItemProcessor implements ItemProcessor<CaseAbstracts, CaseAbstracts> {
+import java.util.List;
+
+@Component
+public class CaseAbstractsItemProcessor implements ItemProcessor<List<CaseAbstracts>, List<CaseAbstracts>> {
 
     private final Logger log = LoggerFactory.getLogger(CaseAbstractsItemProcessor.class);
+    private CaseAbstractsDao caseAbstractsDao;
+
+    public CaseAbstractsItemProcessor(CaseAbstractsDao caseAbstractsDao) {
+        this.caseAbstractsDao = caseAbstractsDao;
+    }
 
     @Override
-    public CaseAbstracts process(CaseAbstracts caseAbstracts) throws Exception {
-        log.info("Processing result: " + caseAbstracts);
-        return caseAbstracts;
+    public List<CaseAbstracts> process(List<CaseAbstracts> caseAbstractsList) throws Exception {
+        List<CaseAbstracts> caseAbstractsList1 = caseAbstractsDao.findAll();
+        log.info("Processing result: " + caseAbstractsList1);
+        return caseAbstractsList1;
     }
 }
--- backend/src/main/java/com/com/backend/config/BatchConfiguration.java
+++ backend/src/main/java/com/com/backend/config/BatchConfiguration.java
@@ -1,23 +1,54 @@
 package com.com.backend.config;
 
+import com.com.backend.batch.CaseAbstractsJobListener;
+import com.com.backend.batch.processor.CaseAbstractsItemProcessor;
+import com.com.backend.batch.reader.CaseAbstractsReader;
+import com.com.backend.batch.writer.CaseAbstractsWriter;
+import com.com.backend.model.CaseAbstracts;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.function.Function;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
-    private JobBuilderFactory jobs;
-    private StepBuilderFactory steps;
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private CaseAbstractsReader caseAbstractsReader;
+    @Autowired
+    private CaseAbstractsItemProcessor caseAbstractsItemProcessor;
+    @Autowired
+    private CaseAbstractsWriter caseAbstractsWriter;
+    @Autowired
+    private CaseAbstractsJobListener caseAbstractsJobListener;
 
-    public BatchConfiguration(JobBuilderFactory jobs, StepBuilderFactory steps) {
-        this.jobs = jobs;
-        this.steps = steps;
+    @Bean(name = "caseAbstracts")
+    public Job job () {
+        Step step = stepBuilderFactory.get("export-to-file-step")
+                .<List<CaseAbstracts>, List<CaseAbstracts>> chunk(100)
+                .reader(caseAbstractsReader)
+                .processor(caseAbstractsItemProcessor)
+                .writer(caseAbstractsWriter)
+                .build();
+
+        return jobBuilderFactory.get("export-to-file")
+                                .incrementer(new RunIdIncrementer())
+                                .listener(caseAbstractsJobListener)
+                                .start(step)
+                                .build();
     }
-
-
 
 }
