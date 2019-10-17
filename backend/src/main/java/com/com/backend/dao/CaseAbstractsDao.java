036df9a194176180b93b970bd2bd284d0fdb3297
Patryk Gogol <patryk.gogol@comarch.com>
2019-10-17 15:27:11 +0200

    batch export reader

--- backend/src/main/java/com/com/backend/dao/CaseAbstractsDao.java
+++ backend/src/main/java/com/com/backend/dao/CaseAbstractsDao.java
@@ -4,13 +4,14 @@ import com.com.backend.model.CaseAbstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CaseAbstractsDao extends JpaRepository<CaseAbstracts, Long> {
+public interface CaseAbstractsDao extends PagingAndSortingRepository<CaseAbstracts, Long>, JpaRepository<CaseAbstracts, Long> {
 
     @Modifying
     @Query("update CaseAbstracts ca set ca.status = :status where ca.id = :caseId")
@@ -22,5 +23,7 @@ public interface CaseAbstractsDao extends JpaRepository<CaseAbstracts, Long> {
     List<CaseAbstracts> getAllByUsersEmail(String email);
 
     int countCaseAbstractsByUsersEmail(String email);
+
+    List<CaseAbstracts> getAllByIdIsNotNull();
 
 }
